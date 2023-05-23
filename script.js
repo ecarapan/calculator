e75ec3f516b42a45a769b71db6a3e227d2ff6609
ecarapan <92954426+ecarapan@users.noreply.github.com>
2023-05-23 10:22:41 -0700

    Create operate function

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
+let numberOne = 0;
+let NumberTwo = 0;
+let operator = '';
+
+function operate(numberOne, numberTwo, operator) {
+    let result;
+    switch (operator) {
+        case '+':
+            result = add(numberOne, numberTwo);
+            break;
+        case '-':
+            result = add(numberOne, numberTwo);
+            break;
+        case '*':
+            result = add(numberOne, numberTwo);
+            break;
+        case '/':
+            result = add(numberOne, numberTwo);
+            break;
+        default:
+            result = 'Select on operation';
+    }
+    return result;
+}
 
 function add(addendOne, addendTwo) {
     return addendOne + addendTwo;
